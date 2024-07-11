45a0420e0978c8cb76a25b966761724fc05ed410
Sahajadi <sahajadilohani123@gmail.com>
2024-07-11 13:42:41 +0545

    landing page complete - mobile

---
+++ src/helper/data.js
@@ -0,0 +1,64 @@
+export const landingFooterLinks = [
+    {
+        id: 1,
+        tittle : "Facebook",
+        link : "https://www.facebook.com/Solukhumbu-109823223642765/",
+
+    },
+    {
+        id : 2,
+        tittle : "x",
+        link : " ",
+        
+    },
+    {
+        id : 3,
+        tittle :"Instagram" ,
+        link : "",
+        
+    },
+    {
+        id : 4,
+        tittle :" " ,
+        link : "",
+        
+    },
+    {
+        id : 5,
+        tittle : " ",
+        link : "",
+        
+    }
+]
+export const landingHeaderLinks = [
+    {
+        id : 1,
+        tittle : "About",
+        link : "#",
+    },
+    {
+        id : 2,
+        tittle : "Tours",
+        link : "#",
+    },
+    {
+        id : 3,
+        tittle : "sales",
+        link : "#"
+    },
+    {
+        id : 4,
+        tittle : "contact",
+        link : "#",
+    },
+    {
+        id : 5,
+        tittle : "sign in",
+        link : "/signin",
+    },
+    {
+        id : 6,
+        tittle : "sign up",
+        link : "/signup",
+    }
+]
